60f68e2530ecf567d0f58d80b40d03514ec554b5
Artem Kel <artkel@gmail.com>
2025-02-21 19:38:16 +0100

    minor changes

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from llama_index.experimental.query_engine import PandasQueryEngine
-
+from prompts import new_prompt, instruction_str
 
 load_dotenv()
 
 population_path = os.path.join("data", "population.csv")
-population_df = pd.read_csv(population_path)
+df = pd.read_csv(population_path)
 
-print(population_df.head())
+# print(population_df.head())
 
-population_query_engine = PandasQueryEngine(df=population_df, verbose=True)
+population_query_engine = PandasQueryEngine(
+                                df=df, 
+                                verbose=True,
+                                instruction_str=instruction_str)
+
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+population_query_engine.query("what is the population of Australia?")
+
+# print(df[df['Country'] == 'Australia']['Population 2024'].values[0])
 
 
