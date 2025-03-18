f21b1a1244d2447eefe994658bf1f2ef19ab2b61
Artem Kel <artkel@gmail.com>
2025-03-18 18:57:02 +0100

    new agentic sales pipeline project added

---
+++ agentics_sales_pipeline/crew.py
@@ -0,0 +1,174 @@
+# Warning control
+import warnings
+warnings.filterwarnings('ignore')
+from data_models import LeadPersonalInfo, CompanyInfo, LeadScore, LeadScoringResult
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool
+import asyncio
+
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+import pandas as pd
+import os
+import yaml
+from crewai import Agent, Task, Crew
+from crewai import Flow
+from crewai.flow.flow import listen, start
+
+os.environ['OPENAI_MODEL_NAME'] = 'gpt-4o-mini'
+
+# Define file paths for YAML configurations
+files = {
+    'lead_agents': 'config/lead_qualification_agents.yaml',
+    'lead_tasks': 'config/lead_qualification_tasks.yaml',
+    'email_agents': 'config/email_engagement_agents.yaml',
+    'email_tasks': 'config/email_engagement_tasks.yaml'
+}
+
+# Load configurations from YAML files
+configs = {}
+for config_type, file_path in files.items():
+    with open(file_path, 'r') as file:
+        configs[config_type] = yaml.safe_load(file)
+
+# Assign loaded configurations to specific variables
+lead_agents_config = configs['lead_agents']
+lead_tasks_config = configs['lead_tasks']
+email_agents_config = configs['email_agents']
+email_tasks_config = configs['email_tasks']
+
+
+### lead qualification crew
+# Creating Agents
+lead_data_agent = Agent(
+  config=lead_agents_config['lead_data_agent'],
+  tools=[SerperDevTool(), ScrapeWebsiteTool()]
+)
+
+cultural_fit_agent = Agent(
+  config=lead_agents_config['cultural_fit_agent'],
+  tools=[SerperDevTool(), ScrapeWebsiteTool()]
+)
+
+scoring_validation_agent = Agent(
+  config=lead_agents_config['scoring_validation_agent'],
+  tools=[SerperDevTool(), ScrapeWebsiteTool()]
+)
+
+# Creating Tasks
+lead_data_task = Task(
+  config=lead_tasks_config['lead_data_collection'],
+  agent=lead_data_agent
+)
+
+cultural_fit_task = Task(
+  config=lead_tasks_config['cultural_fit_analysis'],
+  agent=cultural_fit_agent
+)
+
+scoring_validation_task = Task(
+  config=lead_tasks_config['lead_scoring_and_validation'],
+  agent=scoring_validation_agent,
+  context=[lead_data_task, cultural_fit_task],
+  output_pydantic=LeadScoringResult
+)
+
+# Creating Crew
+lead_scoring_crew = Crew(
+  agents=[
+    lead_data_agent,
+    cultural_fit_agent,
+    scoring_validation_agent
+  ],
+  tasks=[
+    lead_data_task,
+    cultural_fit_task,
+    scoring_validation_task
+  ],
+  verbose=True
+)
+
+
+### Email engagement crew
+# Creating Agents
+email_content_specialist = Agent(
+  config=email_agents_config['email_content_specialist']
+)
+
+engagement_strategist = Agent(
+  config=email_agents_config['engagement_strategist']
+)
+
+# Creating Tasks
+email_drafting = Task(
+  config=email_tasks_config['email_drafting'],
+  agent=email_content_specialist
+)
+
+engagement_optimization = Task(
+  config=email_tasks_config['engagement_optimization'],
+  agent=engagement_strategist
+)
+
+# Creating Crew
+email_writing_crew = Crew(
+  agents=[
+    email_content_specialist,
+    engagement_strategist
+  ],
+  tasks=[
+    email_drafting,
+    engagement_optimization
+  ],
+  verbose=True
+)
+
+
+### FLOW
+class SalesPipeline(Flow):
+    @start()
+    def fetch_leads(self):
+        # Pull our leads from the database
+        leads = [
+            {
+                "lead_data": {
+                    "name": "João Moura",
+                    "job_title": "Director of Engineering",
+                    "company": "Clearbit",
+                    "email": "joao@clearbit.com",
+                    "use_case": "Using AI Agent to do better data enrichment."
+                },
+            },
+        ]
+        return leads
+
+    @listen(fetch_leads)
+    def score_leads(self, leads):
+        scores = lead_scoring_crew.kickoff_for_each(leads)
+        self.state["score_crews_results"] = scores
+        return scores
+
+    @listen(score_leads)
+    def store_leads_score(self, scores):
+        # Here we would store the scores in the database
+        return scores
+
+    @listen(score_leads)
+    def filter_leads(self, scores):
+        return [score for score in scores if score['lead_score'].score > 70]
+
+    @listen(filter_leads)
+    def write_email(self, leads):
+        scored_leads = [lead.to_dict() for lead in leads]
+        emails = email_writing_crew.kickoff_for_each(scored_leads)
+        return emails
+
+    @listen(write_email)
+    def send_email(self, emails):
+        # Here we would send the emails to the leads
+        return emails
+
+flow = SalesPipeline()
+# Try the synchronous version if available
+emails = flow.kickoff()
+
